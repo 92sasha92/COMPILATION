bff64a139e9e4ba3cb4169c250387761178a9c05
92sasha92 <30442331+92sasha92@users.noreply.github.com>
2017-12-21 15:20:40 +0200

    Add files via upload

---
+++ EX3/FOLDER_2_SRC/TYPES/TYPE_ARRAY.java
@@ -0,0 +1,16 @@
+package TYPES;
+
+public class TYPE_ARRAY extends TYPE
+{
+
+	TYPE type;
+	
+	/****************/
+	/* CTROR(S) ... */
+	/****************/
+	public TYPE_ARRAY(String name,TYPE type)
+	{
+		this.name = name;
+		this.type = type;
+	}
+}
