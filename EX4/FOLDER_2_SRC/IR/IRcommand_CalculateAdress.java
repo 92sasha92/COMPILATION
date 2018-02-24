04ef5791067744798c6d084fc358575cd32f449d
aleksandr minchiu <92sasha92@gmail.com>
2018-02-24 22:17:47 +0200

    Calculate Adress IR

---
+++ EX4/FOLDER_2_SRC/IR/IRcommand_CalculateAdress.java
@@ -0,0 +1,28 @@
+package IR;
+
+import MIPS.sir_MIPS_a_lot;
+import Temp.*;
+
+public class IRcommand_CalculateAdress extends IRcommand {
+
+	private Temp varAddress;
+	private Temp offset;
+	
+	public IRcommand_CalculateAdress(Temp varAddress, Temp offset){
+		this.varAddress = varAddress;
+		this.offset = offset;
+	}
+	
+	@Override
+	public void MIPSme() {
+        boolean shouldEnumerate = false;
+        String funcName = getFreshLabel("Calculate_Adress", shouldEnumerate);
+
+        sir_MIPS_a_lot.getInstance().push(varAddress);
+        sir_MIPS_a_lot.getInstance().push(offset);
+        sir_MIPS_a_lot.getInstance().jal(funcName);
+        sir_MIPS_a_lot.getInstance().mixedMove(varAddress, "$v0");
+	}
+
+
+}
