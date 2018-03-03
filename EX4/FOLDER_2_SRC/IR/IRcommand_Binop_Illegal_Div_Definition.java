bb967b8965e993967ddd68601d533c16319d8cab
Camilla Rhodes <camillarhodes@yandex.com>
2018-03-03 12:52:11 +0200

    fixed division string

--- EX4/FOLDER_2_SRC/IR/IRcommand_Binop_Illegal_Div_Definition.java
+++ EX4/FOLDER_2_SRC/IR/IRcommand_Binop_Illegal_Div_Definition.java
@@ -35,7 +35,7 @@ public class IRcommand_Binop_Illegal_Div_Definition extends IRcommand
         }
 
         Temp t = Temp_FACTORY.getInstance().getFreshTemp();
-        sir_MIPS_a_lot.getInstance().load_string(t,"Illegal Division By Zero\n");
+        sir_MIPS_a_lot.getInstance().load_string(t,"Division By Zero\n");
         sir_MIPS_a_lot.getInstance().print_string(t);
         sir_MIPS_a_lot.getInstance().exitProgram();
             for (int i = Temp_FACTORY.reservedTemps.values().length + 7; i >=  Temp_FACTORY.reservedTemps.values().length; i--) {
